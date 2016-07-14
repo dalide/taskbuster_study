3f5f4a2cae18224d69c28ac13e189edc3ecb19af
dalide <xiyao.fei@gmail.com>
2016-07-13 21:40:28 -0500

    Internationalization and localization

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -70,12 +72,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'taskbuster.wsgi.application'
+
 
 
 # Database
@@ -93,8 +97,16 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGE = (
+    ('en', _('English')),
+    ('ca', _('Catalan')),
+    )
 
-TIME_ZONE = 'UTC'
+LOCALE_PATHS=(
+    os.path.join(BASE_DIR, "locale"),
+    )
+
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
